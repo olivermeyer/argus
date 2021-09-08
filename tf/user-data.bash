1a1d52c6c21cd2aa3a967b5d77404efee59f0b52
Oliver Meyer <omeyer@toogoodtogo.com>
2021-09-08 20:58:14 +0200

    Fix user-data.bash

--- tf/user-data.bash
+++ tf/user-data.bash
@@ -4,8 +4,6 @@ sudo su root
 yum install -y docker
 service docker start
 
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 595687261518.dkr.ecr.eu-
-west-1.amazonaws.com
-docker pull 595568261518.dkr.ecr.eu-west-1.amazonaws.com/argus:latest
-docker tag 595568261518.dkr.ecr.eu-west-1.amazonaws.com/argus:latest argus:latest
-
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 595687261518.dkr.ecr.eu-west-1.amazonaws.com
+docker pull 595687261518.dkr.ecr.eu-west-1.amazonaws.com/argus:latest
+docker tag 595687261518.dkr.ecr.eu-west-1.amazonaws.com/argus:latest argus:latest
