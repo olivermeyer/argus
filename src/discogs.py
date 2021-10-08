f0c302fd8cb4a5940a10187d918b1d9b5e316372
Oliver Meyer <omeyer@toogoodtogo.com>
2021-10-08 14:05:06 +0200

    Small changes to get_wantlist_ids

--- src/discogs.py
+++ src/discogs.py
@@ -5,15 +5,16 @@ import discogs_client
 from src.logger import logger
 
 
-def get_wantlist_ids(discogs_token: str) -> List[str]:
+def get_wantlist_ids(discogs_token: str, page_size: int = 100) -> List[str]:
     """
     Returns the IDs in the wantlist for the account linked to the token.
     """
     logger.info("Fetching wantlist")
     discogs = discogs_client.Client(
-        user_agent='Argus',
+        user_agent="Argus",
         user_token=discogs_token
     )
     user = discogs.identity()
     wantlist = user.wantlist
+    wantlist.per_page = page_size
     return [str(item.id) for item in wantlist]
